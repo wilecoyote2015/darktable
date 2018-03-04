add79d7caa47ede2788bf1ff7b6f05eb3a9f7267
Your Name <you@example.com>
2018-03-04 19:14:16 +0100

    Handle noise profiles in rawdenoise_nlmeans

--- src/iop/rawdenoise_nlmeans.c
+++ src/iop/rawdenoise_nlmeans.c
@@ -22,6 +22,7 @@
 #endif
 #include "bauhaus/bauhaus.h"
 #include "common/darktable.h"
+#include "common/noiseprofiles_raw.h"
 #include "control/control.h"
 #include "develop/imageop.h"
 #include "develop/imageop_math.h"
@@ -59,9 +60,11 @@ typedef struct dt_iop_rawdenoise_nlmeans_gui_data_t
   GtkWidget *neighborhood_size;
   GtkWidget *patch_size;
   GtkWidget *h;
-//
-//  dt_noiseprofile_t interpolated; // don't use name, maker or model, they may point to garbage
-//  GList *profiles;
+
+  // for noise profile
+  GtkWidget *profile;
+  dt_noiseprofile_raw_t interpolated; // don't use name, maker or model, they may point to garbage
+  GList *profiles;
 
   GtkWidget *label_non_raw;
 } dt_iop_rawdenoise_nlmeans_gui_data_t;
@@ -71,11 +74,16 @@ typedef struct dt_iop_rawdenoise_nlmeans_data_t
   float neighborhood_size;
   float patch_size;
   float h;
+
+  float a[36], b[36];
 } dt_iop_rawdenoise_nlmeans_data_t;
 
 typedef struct dt_iop_rawdenoise_nlmeans_global_data_t
 {
 } dt_iop_rawdenoise_nlmeans_global_data_t;
+
+// todo: is the declaration here so that it can be used?
+static dt_noiseprofile_raw_t dt_iop_rawdenoise_nlmeans_get_auto_profile(dt_iop_module_t *self);
 
 const char *name()
 {
@@ -433,6 +441,47 @@ void reload_defaults(dt_iop_module_t *module)
     module->hide_enable_button = 1;
   module->default_enabled = 0;
 
+  dt_iop_rawdenoise_nlmeans_gui_data_t *g = (dt_iop_rawdenoise_nlmeans_gui_data_t *)module->gui_data;
+
+  // get matching profiles:
+  char name[512];
+  if(g->profiles) g_list_free_full(g->profiles, dt_noiseprofile_raw_free);
+  g->profiles = dt_noiseprofile_raw_get_matching(&module->dev->image_storage);
+  g->interpolated = dt_noiseprofile_raw_generic; // default to generic poissonian
+  g_strlcpy(name, _(g->interpolated.name), sizeof(name));
+
+  const int iso = module->dev->image_storage.exif_iso;
+  dt_noiseprofile_raw_t *last = NULL;
+  for(GList *iter = g->profiles; iter; iter = g_list_next(iter))
+  {
+    dt_noiseprofile_raw_t *current = (dt_noiseprofile_raw_t *)iter->data;
+
+    if(current->iso == iso)
+    {
+      g->interpolated = *current;
+      // signal later autodetection in commit_params:
+      g->interpolated.a[0] = -1.0;
+      snprintf(name, sizeof(name), _("found match for ISO %d"), iso);
+      break;
+    }
+    if(last && last->iso < iso && current->iso > iso)
+    {
+      dt_noiseprofile_raw_interpolate(last, current, &g->interpolated);
+      // signal later autodetection in commit_params:
+      g->interpolated.a[0] = -1.0;
+      snprintf(name, sizeof(name), _("interpolated from ISO %d and %d"), last->iso, current->iso);
+      break;
+    }
+    last = current;
+  }
+
+  dt_bauhaus_combobox_add(g->profile, name);
+  for(GList *iter = g->profiles; iter; iter = g_list_next(iter))
+  {
+    dt_noiseprofile_raw_t *profile = (dt_noiseprofile_raw_t *)iter->data;
+    dt_bauhaus_combobox_add(g->profile, profile->name);
+  }
+
 end:
   memcpy(module->params, &tmp, sizeof(dt_iop_rawdenoise_nlmeans_params_t));
   memcpy(module->default_params, &tmp, sizeof(dt_iop_rawdenoise_nlmeans_params_t));
@@ -469,6 +518,21 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev
   d->neighborhood_size = (int)p->neighborhood_size;
   d->h = p->h;
 
+  // get the profile parameters
+  // compare if a[0] in params is set to "magic value" -1.0 for autodetection
+  if(p->a[0] == -1.0)
+  {
+    // autodetect matching profile again, the same way as detecting their names,
+    // this is partially duplicated code and data because we are not allowed to access
+    // gui_data here ..
+    dt_noiseprofile_raw_t interpolated = dt_iop_rawdenoise_nlmeans_get_auto_profile(self);
+    for(int k = 0; k < 36; k++)
+    {
+      d->a[k] = interpolated.a[k];
+      d->b[k] = interpolated.b[k];
+    }
+  }
+
 
   if (!(pipe->image.flags & DT_IMAGE_RAW))
     piece->enabled = 0;
@@ -495,7 +559,53 @@ void gui_update(dt_iop_module_t *self)
   dt_bauhaus_slider_set(g->neighborhood_size, p->neighborhood_size);
   dt_bauhaus_slider_set(g->h, p->h);
 
+  dt_bauhaus_combobox_set(g->profile, -1);
+  if(p->a[0] == -1.0)
+  {
+    dt_bauhaus_combobox_set(g->profile, 0);
+  }
+  else
+  {
+    int i = 1;
+    for(GList *iter = g->profiles; iter; iter = g_list_next(iter), i++)
+    {
+      dt_noiseprofile_raw_t *profile = (dt_noiseprofile_raw_t *)iter->data;
+      if(!memcmp(profile->a, p->a, sizeof(float) * 3)
+         && !memcmp(profile->b, p->b, sizeof(float) * 3))
+      {
+        dt_bauhaus_combobox_set(g->profile, i);
+        break;
+      }
+    }
+  }
+
   gtk_stack_set_visible_child_name(GTK_STACK(g->stack), self->hide_enable_button ? "non_raw" : "raw");
+}
+
+static dt_noiseprofile_raw_t dt_iop_rawdenoise_nlmeans_get_auto_profile(dt_iop_module_t *self)
+{
+  GList *profiles = dt_noiseprofile_raw_get_matching(&self->dev->image_storage);
+  dt_noiseprofile_raw_t interpolated = dt_noiseprofile_raw_generic; // default to generic poissonian
+
+  const int iso = self->dev->image_storage.exif_iso;
+  dt_noiseprofile_raw_t *last = NULL;
+  for(GList *iter = profiles; iter; iter = g_list_next(iter))
+  {
+    dt_noiseprofile_raw_t *current = (dt_noiseprofile_raw_t *)iter->data;
+    if(current->iso == iso)
+    {
+      interpolated = *current;
+      break;
+    }
+    if(last && last->iso < iso && current->iso > iso)
+    {
+      dt_noiseprofile_raw_interpolate(last, current, &interpolated);
+      break;
+    }
+    last = current;
+  }
+  g_list_free_full(profiles, dt_noiseprofile_raw_free);
+  return interpolated;
 }
 
 static void patch_size_callback(GtkWidget *slider, gpointer user_data)
@@ -522,6 +632,21 @@ static void h_callback(GtkWidget *slider, gpointer user_data)
   if(self->dt->gui->reset) return;
   dt_iop_rawdenoise_nlmeans_params_t *p = (dt_iop_rawdenoise_nlmeans_params_t *)self->params;
   p->h = dt_bauhaus_slider_get(slider);
+  dt_dev_add_history_item(darktable.develop, self, TRUE);
+}
+
+static void profile_callback(GtkWidget *w, dt_iop_module_t *self)
+{
+  int i = dt_bauhaus_combobox_get(w);
+  dt_iop_rawdenoise_nlmeans_params_t *p = (dt_iop_rawdenoise_nlmeans_params_t *)self->params;
+  dt_iop_rawdenoise_nlmeans_gui_data_t *g = (dt_iop_rawdenoise_nlmeans_gui_data_t *)self->gui_data;
+  const dt_noiseprofile_raw_t *profile = &(g->interpolated);
+  if(i > 0) profile = (dt_noiseprofile_raw_t *)g_list_nth_data(g->profiles, i - 1);
+  for(int k = 0; k < 3; k++)
+  {
+    p->a[k] = profile->a[k];
+    p->b[k] = profile->b[k];
+  }
   dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
 
@@ -557,6 +682,14 @@ void gui_init(dt_iop_module_t *self)
   dt_bauhaus_widget_set_label(g->h, NULL, _("filter strength"));
   g_signal_connect(G_OBJECT(g->h), "value-changed", G_CALLBACK(h_callback), self);
 
+  // profile
+  // todo: profile callback
+  g->profiles = NULL;
+  g->profile = dt_bauhaus_combobox_new(self);
+  gtk_box_pack_start(GTK_BOX(self->widget), g->profile, TRUE, TRUE, 0);
+  dt_bauhaus_widget_set_label(g->profile, NULL, _("profile"));
+  g_signal_connect(G_OBJECT(g->profile), "value-changed", G_CALLBACK(profile_callback), self);
+
   gtk_widget_show_all(g->box_raw);
   gtk_stack_add_named(GTK_STACK(g->stack), g->box_raw, "raw");
 
@@ -571,6 +704,8 @@ void gui_init(dt_iop_module_t *self)
 
 void gui_cleanup(dt_iop_module_t *self)
 {
+  dt_iop_rawdenoise_nlmeans_gui_data_t *g = (dt_iop_rawdenoise_nlmeans_gui_data_t *)self->gui_data;
+  g_list_free_full(g->profiles, dt_noiseprofile_raw_free);
   free(self->gui_data);
   self->gui_data = NULL;
 }
